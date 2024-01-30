173af2ddc4f96e4a410928e523d253886726c12d
lorensk1ar <LorenSklar@outlook.com>
2024-01-30 15:56:33 -0500

    Implements classes horsescraper.py
    This version uses classes for provider, scrape, session, and section
    
    Also, now outputs to file rather than to shell
    
    NB: this versions assumes 5 seats are available if class is not full & assumes length of each session is 2 hours

--- horsescraper.py
+++ horsescraper.py
@@ -1,114 +1,86 @@
+'''
+## NOTES
+assumes 5 seats are available if class is not full
+assumes length of session is 2 hours
+
+
+## TO DO
+construct separate json objects if more than 100 sections
+construct dictionary using for key, value in vars(self).items():
+short course name up to first comma?
+parse notes
+parse teacher better including multiple teachers?
+parse enrollment close
+some sites expose a reliable SKU, UUID, or other identifier?
+
+'''
+
 ## LIBRARIES
-from datetime import datetime
 from horselibrary import *
 
 ## FUNCTIONS
-def get_actors_connection(session):
-    # set url and tags for this provider
-    provider_url = 'https://www.actorsconnection.com/classes/'
-    provider_navigation_tag = 'ul.pagination'
-    provider_course_tag = 'div.listing.clearfix'
-    section_id_tag = 'h1'
-    section_name_tag = 'h1'
-    section_available_seats_tag = 'p.full-class'
-    section_price_tag = "div.price"
-
-    # create new provider dictionary
-    provider = "actorsconnection.com"
-    records = "all except seminars"
+def scrape_provider(provider: dict):
+    # open html session
+    html_session = HTMLSession()
 
     # get time stamp
-    tmp = datetime.now()
-    timestamp = tmp.strftime("%Y-%m-%d %H:%M:%S")
-    purge_before = timestamp
-
-    scrape_key_value_pairs = [
-        ("provider", provider),
-        ("records", records),
-        ("purge_before", purge_before),
-        ("sections", []),  
-        ("scrape_notices", [])
-    ]
-    
-    new_scrape = create_new_dictionary(scrape_key_value_pairs)
+    purge_before = get_timestamp("%Y-%m-%d %H:%M:%S")
 
     # get page links
-    page_urls = get_page_urls(session, provider_url, provider_navigation_tag)
+    page_list_container = get_container_from_html(
+        html_session, provider.url,
+        provider.nav_tag,
+        )
 
-    # remove first & last
-    page_urls = page_urls[1:-1]
+    raw_urls = extract_from_container(
+        page_list_container,
+        tag = 'a',
+        attr = 'href')
 
-    # add https to url
-    page_urls = list(map(lambda url: "https:" + url, page_urls))
-    
+    page_urls = clean_urls(raw_urls, 1, -1, prefix  = "https:")
+
     # loop through pages
-    section_urls = []
+    raw_urls = []
     for page_url in page_urls:
         # get section links
-        some_urls = get_section_urls(session, page_url, provider_course_tag)
+        section_container_list = get_container_list_from_html(
+            html_session,
+            page_url,
+            provider.section_url_tag,
+        )
 
-        # add to list
-        section_urls += some_urls
+        some_urls = extract_from_container_list(
+            section_container_list,
+            tag = 'a',
+            attr = 'href')
+        raw_urls += some_urls
 
-    # add https to url
-    section_urls = list(map(lambda url: "https:" + url, section_urls))
-    
-    # loop through section links
+    section_urls = clean_urls(raw_urls, prefix  = "https:")
+
+    # loop through sections
+    sections_list = []
     for section_url in section_urls:
+        print(section_url)
+        
         # get section data
-        section_data = session.get(section_url)
-        
-        # update session
-        session_date = ""
-        session_start_time = ""
-        session_end_time = ""
-        
-        session_key_value_pairs = [
-            ("date", session_date),
-            ("start_time", session_start_time),
-            ("end_time", session_end_time)
-        ]
-        
-        new_session = create_new_dictionary(session_key_value_pairs)
+        response = html_session.get(section_url)
 
-        # update location
-        location_name = ""
-        location_street = ""
-        location_street2 = ""
-        location_city = ""
-        location_state = ""
-        location_zip = ""
-        location_cross = ""
-        
-        location_key_value_pairs = [
-            ("name", location_name),
-            ("street_address", location_street),
-            ("street_address2", location_street2),
-            ("city", location_city),
-            ("state", location_state),
-            ("zip", location_zip),
-            ("cross_streets", location_cross)
-        ]
-       
-        new_location = create_new_dictionary(location_key_value_pairs)
+        # parse course id
+        section_id = response.html.find(provider.section_id_tag, first = True).attrs["content"]
 
-        # update section
-        # section id
-        section_id = section_data.html.find(section_id_tag, first=True).text
-        
-        # section name
-        section_name = section_data.html.find(section_name_tag, first=True).text
+        # parse course name
+        section_name = response.html.find(provider.section_name_tag, first = True).attrs["content"]
 
-        # available seats
-        section_full = section_data.html.find(section_available_seats_tag, first=True)
+        # parse available seats
+        section_full = response.html.find(provider.section_available_tag, first = True)
 
         if section_full:
             available_seats = 0
         else:
             available_seats = 5
         
-        # section price
-        price_text = section_data.html.find(section_price_tag, first=True).text
+        # parse price
+        price_text = response.html.find(provider.section_price_tag, first = True).text
 
         if price_text == "Price: Free":
             section_price = float(0)
@@ -121,63 +93,106 @@ def get_actors_connection(session):
             except:
                 section_price = "N/A"
 
-        section_key_value_pairs = [
-            ("provider_course_id", section_id),
-            ("course_name", section_name),
-            ("url", section_url),
-            ("available_seats", available_seats),
-            ("price", section_price),
-            ("sessions", [new_session]),
-            ("location_type", "default"),
-            ("location", new_location),
-            ("notes", ""),
-            ("teacher", ""),
-            ("enrollment_close_datetime", "")
-        ]
-        
-        new_section = create_new_dictionary(section_key_value_pairs)
+        # parse sessions
+        sessions_list = []
+        sessions_html = response.html.find('td')
+        sessions_text = [item.text for item in sessions_html]
+        item_count = len(sessions_text)
+        for i in range(0, item_count, 2):
+            date_text = sessions_text[i]
+            date_value = datetime.strptime(date_text, "%A, %B %d, %Y").date()
+            session_date = date_value.strftime("%Y-%m-%d")
+            
+            start_time_text = sessions_text[i+1]
+            start_time_value = datetime.strptime(start_time_text, "%I:%M %p").time()
+            session_start_time = start_time_value.strftime("%H:%M:%S")
 
-        # update scrape notice
-        scrape_notice_text = ""
-        scrape_notice_url = section_url
+            end_time_value = (datetime.combine(datetime.min, start_time_value) + timedelta(hours=2)).time()
+            session_end_time = end_time_value.strftime("%H:%M:%S")
+            
+            # construct session object
+            new_session = Session(session_date, session_start_time, session_end_time)
 
-        scrape_notice_key_value_pairs = [
-            ("notice", scrape_notice_text),
-            ("url", scrape_notice_url)
-        ]
+            # add to sessions list
+            sessions_list.append(new_session)
 
-        new_scrape_notice = create_new_dictionary(scrape_notice_key_value_pairs)
+        # parse location
 
-        # add section and scrape notice if not seminar
+        # parse notes
+
+        # parse teacher
+        tmp = response.html.find(provider.section_teacher_tag)
+        session_teacher = tmp[-1].text
+
+        # parse enrollment close
+
+        # construct section object
+        new_section = Section(
+            section_id,
+            section_name,
+            section_url,
+            available_seats,
+            section_price,
+            sessions = sessions_list,
+            teacher = session_teacher
+        )
+
+        # check if section is valid & if valid add to sections list
+        valid = True
         section_name_lowercase = section_name.lower()
-        index = section_name_lowercase.find("seminar")
-        if index == -1:
-            if new_scrape['sections'] == []:
-                new_scrape['sections'] = [new_section]
-            else:
-                new_scrape['sections'].append(new_section)
+        if provider.remove_if:
+            for item in provider.remove_if:
+                item_lowercase = item.lower()
+                index = section_name_lowercase.find(item_lowercase)
+                if index >= 0:
+                    valid = False
+                    break
 
-            if len(scrape_notice_text) == 0:
-                   pass
-            elif new_scrape['scrape_notices'] == []:
-                new_scrape['scrape_notices'] = [new_scrape_notice]
-            else:
-                new_scrape['scrape_notices'].append(new_scrape_notice)
+            if valid:
+                sections_list.append(new_section)
 
-    # convert provider object to json object
-    scrape_json = json.dumps(new_scrape)
+    # close html session
+    html_session.close()
+
+    # construct scrape object
+    new_scrape = Scrape(
+        provider.name,
+        provider.records,
+        purge_before,
+        sections_list, 
+    )
+
+    # convert scrape object to json object
+    new_scrape_dictionary = new_scrape.get_dictionary()
+    scrape_json = json.dumps(new_scrape_dictionary)
 
     # endpoint = "https://horseshoe.coursehorse.com/sync"
     # post_json_data(endpoint, scrape_json)
-    print(scrape_json)
-    print("\n")
+    # indented_json = json.dumps(new_scrape_dictionary, indent=2)
+    # print(indented_json)
 
-def get_all_providers():
-    # initiate session session
-    session = HTMLSession()
+    print(f'{len(new_scrape.sections)} sections found')
 
-    # gather data
-    get_actors_connection(session)
+    file_path = "test.json"
+    with open(file_path, 'w') as json_file:
+        json.dump(scrape_json, json_file)
+
+## PROVIDERS
+actorsconnection = Provider(
+    name = "Actors Connection",
+    url = 'https://www.actorsconnection.com/classes/',
+    records = "all except seminars",
+    nav_tag = 'ul.pagination',
+    section_url_tag = 'div.listing.clearfix',
+    section_id_tag = 'meta[property="og:title"]',
+    section_name_tag = 'meta[property="og:title"]',
+    section_available_tag = 'p.full-class',
+    section_price_tag = 'div.price',
+    section_teacher_tag = 'h2',
+    remove_if = ["seminar"]
+)
 
 ## MAIN
-get_all_providers()
+provider_list = [actorsconnection]
+for provider in provider_list:
+    scrape_provider(provider)
